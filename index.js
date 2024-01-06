5fbd7edce7aa894ed9681aac17ade6b978682356
anfal11 <anfal15-14303@diu.edu.bd>
2024-01-06 19:14:07 +0600

    payment ssllcommerz added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const SSLCommerzPayment = require('sslcommerz-lts')
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
@@ -27,6 +28,11 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
+const store_id = process.env.STORE_ID;
+const store_passwd = process.env.STORE_PASS;
+const is_live = false //true for live, false for sandbox
+
 
 async function run() {
   try {
@@ -88,7 +94,68 @@ async function run() {
         return res.status(500).send({ message: "Internal server error" });
       }
     };
+   
+    const tran_id = new ObjectId().toString();
+    //payment
+    app.post('/payment', async (req, res) => {
+      // Assuming req.body contains the necessary information, including the property price
+      const property = await propertyCollection.findOne({
+          _id: new ObjectId(req.body.id),
+      });
+  
+      console.log('Property:', property);
+      if (!property) {
+        console.log('Property not found for ID:', req.body.id);
+        return res.status(404).send("Property not found");
+     }
 
+     
+    const pay = req.body
+     
+  
+      const data = {
+          total_amount: property.price || 0, // Set total_amount dynamically based on property price
+          currency: 'BDT',
+          tran_id: tran_id, // use unique tran_id for each api call
+          success_url: 'http://localhost:5000/success',
+          fail_url: 'http://localhost:5000/fail',
+          cancel_url: 'http://localhost:5000/cancel',
+          ipn_url: 'http://localhost:5000/ipn',
+          shipping_method: 'Courier',
+          product_name: property.title,
+          product_category: 'Electronic',
+          product_profile: 'general',
+          cus_name: pay.name,
+          cus_email: pay.email,
+          cus_add1: pay.address,
+          cus_add2: 'Dhaka',
+          cus_city: 'Dhaka',
+          cus_state: 'Dhaka',
+          cus_postcode: '1000',
+          cus_country: 'Bangladesh',
+          cus_phone: '01711111111',
+          cus_fax: '01711111111',
+          ship_name: 'Customer Name',
+          ship_add1: 'Dhaka',
+          ship_add2: 'Dhaka',
+          ship_city: 'Dhaka',
+          ship_state: 'Dhaka',
+          ship_postcode: 1000,
+          ship_country: 'Bangladesh',
+      };
+  
+      console.log('Data:', data);
+  
+      // Uncomment the following code when you are ready to integrate with SSLCommerzPayment
+      const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+      sslcz.init(data).then(apiResponse => {
+          // Redirect the user to the payment gateway
+          let GatewayPageURL = apiResponse.GatewayPageURL
+          res.redirect(GatewayPageURL)
+          console.log('Redirecting to: ', GatewayPageURL)
+      });
+  });
+  
     //user related api
     app.get("/api/v1/users", async (req, res) => {
       const users = await userCollection.find().toArray();
